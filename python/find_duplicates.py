a62884dd330a13c5d9a0a50829d4c0b4d16fc8b0
alisalofe <salome@Salome.>
2025-09-10 18:14:36 -0400

    Added find_duplicates_dictionary to find_duplicates.py

--- python/find_duplicates.py
+++ python/find_duplicates.py
@@ -6,6 +6,16 @@ def find_duplicates_nested_loop(l: list) -> list:
                 s.add(l[i])
     return list(s)
 
+def find_duplicates_dictionary(l: list) -> list:
+    d = {}
+    s = set()
+    for n in l:
+        if n in d:
+            s.add(n)
+        else:
+            d[n] = 0
+    return list(s)
+
 # In Python, if __name__ == "__main__" is a conditional check that determines whether 
 # a Python file is being run as the main program or imported as a module into another 
 # script.
@@ -19,3 +29,7 @@ if __name__ == "__main__":
     print("Sample 2:", find_duplicates_nested_loop(sample2))
     print("Sample 3:", find_duplicates_nested_loop(sample3))
     print("Sample 4:", find_duplicates_nested_loop(sample4))
+    print("Sample 1:", find_duplicates_dictionary(sample1))
+    print("Sample 2:", find_duplicates_dictionary(sample2))
+    print("Sample 3:", find_duplicates_dictionary(sample3))
+    print("Sample 4:", find_duplicates_dictionary(sample4))
